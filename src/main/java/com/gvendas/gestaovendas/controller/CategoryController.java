5f2bc4f9e6a19e8678e9502b916c46867616d811
Gabriel Rabelo <gabriel872003@gmail.com>
2021-06-19 16:17:39 -0300

    Atualizando categoria no banco de dados

--- src/main/java/com/gvendas/gestaovendas/controller/CategoryController.java
+++ src/main/java/com/gvendas/gestaovendas/controller/CategoryController.java
@@ -3,17 +3,22 @@ package com.gvendas.gestaovendas.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gvendas.gestaovendas.entities.Category;
+import com.gvendas.gestaovendas.repository.CategoryRepository;
 import com.gvendas.gestaovendas.service.CategoryService;
 
 @RestController
@@ -38,5 +43,10 @@ public class CategoryController {
 	public ResponseEntity<Category> save(@RequestBody Category category){
 		Category categorySaved = categoryService.save(category);
 		return ResponseEntity.status(HttpStatus.CREATED).body(categorySaved);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Category> update(@PathVariable Long id,@RequestBody Category category) {
+		return ResponseEntity.ok(categoryService.update(id, category));
 	}
 }
--- src/main/java/com/gvendas/gestaovendas/service/CategoryService.java
+++ src/main/java/com/gvendas/gestaovendas/service/CategoryService.java
@@ -3,7 +3,9 @@ package com.gvendas.gestaovendas.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.gvendas.gestaovendas.entities.Category;
@@ -25,5 +27,19 @@ public class CategoryService {
 	
 	public Category save(Category category) {
 		return categoryRepository.save(category);
+	}
+	
+	public Category update(Long id, Category category) {
+		Category categorySave = validateCategoryExists(id);
+		BeanUtils.copyProperties(category, categorySave, "id");
+		return categoryRepository.save(categorySave);
+	}
+
+	private Category validateCategoryExists(Long id) {
+		Optional<Category> category = searchById(id);
+		if(category.isEmpty()) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		return category.get();
 	}
 }
